3a894c60615ae6e212ba4deb52b7d15e95194161
Danyal Saeed <cherrylotter@gmail.com>
2025-07-31 15:59:09 -0500

    main/add-tasks

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,12 +8,14 @@ import { createHomeStyles } from '@/assets/styles/home.styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {LinearGradient} from "expo-linear-gradient";
 import Header from '@/components/Header';
+import TodoInput from '@/components/TodoInput';
 
 
 export default function Index() {
   const {toggleDarkMode, colors} = useTheme();
 
   const homeStyles = createHomeStyles(colors);
+  const todos = useQuery(api.todos.getTodos);
   
 
   return (
@@ -21,6 +23,7 @@ export default function Index() {
       <StatusBar barStyle={colors.statusBarStyle}/>
     <SafeAreaView style={homeStyles.safeArea}>
       <Header/>
+      <TodoInput/>
       <TouchableOpacity onPress={toggleDarkMode}><Text>Toggle the mode</Text></TouchableOpacity>
      </SafeAreaView>
     </LinearGradient>
